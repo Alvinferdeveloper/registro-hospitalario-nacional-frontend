b13607a5a758fb7d3a0dea783a4c5f79b996af66
Albin <alvinfer67@gmail.com>
2024-08-31 19:31:03 -0600

    added: Texto para error en el input de cedula requerida

--- src/app/utils/validationSchemas.ts
+++ src/app/utils/validationSchemas.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 export const userFormRegisterSchema = yup.object({
     name: yup.string().required('Por favor ingrese su nombre'),
     lastName: yup.string().required('Por favor ingrese su apellido'),
-    identification: yup.string().required().min(16, 'Formato de cedula invalido'),
+    identification: yup.string().required('La cedula es requerida').min(16, 'Formato de cedula invalido'),
     municipio: yup.string().required('Por favor ingrese su municipio'),
     city: yup.string().required('Por favor ingrese su ciudad'),
     address:yup.string(),
