299450661b1c914cf1a43f9fe7e7b2592c6bf543
Albin <alvinfer67@gmail.com>
2024-09-19 15:43:47 -0600

    fix: adding the missing authentication token to the request in fetchPatientByHealhCarer

--- src/app/actions/patients.ts
+++ src/app/actions/patients.ts
@@ -1,7 +1,21 @@
 "use server";
 import axios from '@/app/libs/axiosServerConfig'
+import { cookies } from 'next/headers';
 
 export async function fetchPatients() {
-    const res = await axios.get('/patient/getPatients');
+    const res = await axios.get('/patient/getPatients', {
+        headers: {
+            Authorization: cookies().get('access_token')?.value ? 'Bearer ' + cookies().get('access_token')?.value : null,
+        }
+    });
+    return res.data;
+}
+
+export async function fetchPatientByHealhCarer(patientId: string){
+    const res = await axios.get(`/patient/getPatientByHealthCarer/${patientId}`, {
+        headers: {
+            Authorization: cookies().get('access_token')?.value ? 'Bearer ' + cookies().get('access_token')?.value : null,
+        }
+    } );
     return res.data;
 }
