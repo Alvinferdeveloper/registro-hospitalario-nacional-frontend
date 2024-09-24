452695efca21c7dfac27563abcf5c10aa75d8e0d
Albin <alvinfer67@gmail.com>
2024-09-23 20:07:54 -0600

    fix: wrong properties of jsx elements

--- src/app/components/ui/Consultations.tsx
+++ src/app/components/ui/Consultations.tsx
@@ -2,7 +2,7 @@ import { CalendarIcon, StethoscopeIcon, FileTextIcon } from "lucide-react"
 import { ConsultationResponse } from "@/app/types/responseTypes"
 export default async function Consultations({ consultations }: { consultations: ConsultationResponse[]}) {
       return (
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto p-4 mt-[60px]">
           <h1 className="text-3xl font-bold mb-6 text-gray-800">Lista de Expedientes</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {consultations.map((consultation) => (
