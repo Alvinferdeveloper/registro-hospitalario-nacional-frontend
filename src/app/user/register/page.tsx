9df9dfdb29c03b0a4f1d9ec4f896c79e3cf253f3
Albin <alvinfer67@gmail.com>
2024-09-27 23:13:38 -0600

    update: getting departaments from api

--- src/app/user/register/page.tsx
+++ src/app/user/register/page.tsx
@@ -1,14 +1,13 @@
 import UserFormRegister from "@/app/components/ui/user/userFormRegister";
-import { supabase } from "@/app/libs/supabaseClient";
+import axios from "@/app/libs/axiosClientConfig";
 
 interface Departament{
   id:number;
   name:string;
 }
 export default async function Register() {
-  let { data: departaments} = await supabase
-  .from('departaments')
-  .select('id,name');
+  const res = await axios.get('/departament/getDepartaments');
+  const departaments = res.data;
   return (
     <div className=" w-[100vw] flex justify-center md:h-screen items-center flex-col">
       <UserFormRegister departaments={departaments as Departament[]}/>
